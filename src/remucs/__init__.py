a47fc13110b8b34b187c13d86d9c02473337e8f6
Jürgen Hock <juergen.hock@jurihock.de>
2024-03-23 22:38:29 +0100

    Handle empty __package__

--- src/remucs/__init__.py
+++ src/remucs/__init__.py
@@ -1,5 +1,6 @@
 import importlib.metadata
 
-__version__ = importlib.metadata.version(__package__)
+__version__ = importlib.metadata.version(__package__) \
+              if __package__ else None
 
 from remucs.remucs import remucs
