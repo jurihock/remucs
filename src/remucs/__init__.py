0ed861fb67337d64d805092c60d3440767e1c125
Jürgen Hock <juergen.hock@jurihock.de>
2024-03-23 22:57:27 +0100

    Revert "Handle empty __package__"
    This reverts commit a47fc13110b8b34b187c13d86d9c02473337e8f6.

--- src/remucs/__init__.py
+++ src/remucs/__init__.py
@@ -1,6 +1,5 @@
 import importlib.metadata
 
-__version__ = importlib.metadata.version(__package__) \
-              if __package__ else None
+__version__ = importlib.metadata.version(__package__)
 
 from remucs.remucs import remucs
