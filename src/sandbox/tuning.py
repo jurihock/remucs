3f68ebd6a2d2d9fed2c5bc8795e59da83f6f1fc7
Jürgen Hock <juergen.hock@jurihock.de>
2024-03-30 12:50:11 +0100

    Use histogram approach

--- src/sandbox/tuning.py
+++ src/sandbox/tuning.py
@@ -1,4 +1,6 @@
 # pylint: disable=import-error
+# pylint: disable=too-many-locals
+# pylint: disable=too-many-statements
 # pylint: disable=fixme
 
 import matplotlib.pyplot as plot
@@ -7,7 +9,9 @@ import numpy.lib.stride_tricks as tricks
 import scipy.signal
 import soundfile
 
-from qdft import Chroma
+from qdft import QDFT
+from qdft.fafe import QFAFE
+from qdft.scale import Scale
 from synth import synth
 
 np.set_printoptions(suppress=True)
@@ -66,37 +70,44 @@ def main():
     samples.resize(length)
     print(f'new length {len(samples)} {len(samples)/samplerate}s')
 
-    chunks = tricks.sliding_window_view(samples, samplerate)[::samplerate]
-    chroma = Chroma(samplerate, decibel=False, feature='hz')
+    scale = Scale()
+    bw    = (scale.frequency('A0'), scale.frequency('C#8'))
+    qdft  = QDFT(samplerate=samplerate, bandwidth=bw, resolution=12*4)
 
-    chromagram = np.empty((0, chroma.size))
+    chunks = tricks.sliding_window_view(samples, samplerate)[::samplerate]
+    data   = np.empty((0, qdft.size))
 
     for i, chunk in enumerate(chunks):
 
         if not i:
             print('0%')
 
-        chromagram = np.vstack((chromagram, chroma.chroma(chunk)))
+        data = np.vstack((data, qdft.qdft(chunk)))
 
         print(f'{int(100 * (i + 1) / len(chunks))}%')
 
-    # TODO use chroma.qdft.latencies in the next qdft release
-    latency = int(np.max(chroma.qdft.periods[0] - chroma.qdft.offsets))
+    fafe  = QFAFE(qdft)
+    mags  = np.abs(data)
+    freqs = fafe.hz(data)
+    data  = mags + 1j * freqs
+
+    # TODO use qdft.latencies in the next qdft release
+    latency = int(np.max(qdft.periods[0] - qdft.offsets))
     print(f'max. qdft latency {latency}')
 
-    print(f'old shape {chromagram.shape}')
-    chromagram = chromagram[latency:-samplerate]
-    print(f'new shape {chromagram.shape}')
+    print(f'old shape {data.shape}')
+    data = data[latency:-samplerate]
+    print(f'new shape {data.shape}')
 
-    cp0 = chroma.concertpitch
-    cp1 = np.full(len(chromagram), cp0, float)
+    cp0 = scale.concertpitch
+    cp1 = np.full(len(data), cp0, float)
 
-    r = np.real(chromagram)
-    f = np.imag(chromagram)
+    r = np.real(data)
+    f = np.imag(data)
 
-    i = np.arange(len(chromagram))
+    i = np.arange(len(data))
     j = findpeaks(r, 3)
-    k = 1  # int(100e-3 * samplerate)
+    k = 1 # int(100e-3 * samplerate)
 
     for n, m in zip(i, j):
 
@@ -112,24 +123,58 @@ def main():
         cp1[n] = np.sum(a * b) / np.sum(c)
         cp1[n] = cp1[n-1] if np.isnan(cp1[n]) else cp1[n]
 
-    # TODO try a lowpass instead
     cp2 = smooth_savgol(cp1, 100e-3, samplerate)
     cp3 = smooth_polyfit(cp2, 1)
 
-    # TODO improve estimation precision
-    res = np.round([
-        cp3[0],
-        cp3[-1],
-        np.mean(cp3),
-        np.median(cp3)
-    ])
+    vals = np.round(cp1).astype(int)
+    vmin = np.min(vals)
+    vmax = np.max(vals)
+    bins = np.arange(vmin, vmax + 1)
+    edge = np.arange(vmin, vmax + 2) - 0.5
+    weig = np.prod(r[i[..., None], j], axis=-1)
+    hist = np.histogram(vals, bins=edge, weights=weig)
+    hmax = np.argmax(hist[0])
+    bmax = bins[hmax]
+    assert hist[0].shape == bins.shape
+    assert hist[1].shape == edge.shape
 
-    print(f'fist {res[0]} last {res[1]} avg {res[2]} med {res[3]}')
+    res = np.round([
+        cp1[0],
+        cp1[-1],
+        np.min(cp1),
+        np.max(cp1),
+        np.mean(cp1),
+        np.median(cp1),
+        bmax
+    ]).astype(int)
+
+    lab = [
+        'first',
+        'last',
+        'min',
+        'max',
+        'avg',
+        'med',
+        'hist',
+    ]
+
+    print('\n'.join(map(str, zip(lab, res))))
+
+    plot.figure(test + ' hist')
+    plot.hist(vals, bins=edge)
+    plot.axvline(bmax, label='bmax', linestyle='--')
+    plot.axvline(cp, label='cp', linestyle='--', color='m')
+    plot.legend()
+    plot.tight_layout()
 
     plot.figure(test)
-    plot.plot(cp1)
-    plot.plot(cp2, '--')
-    plot.plot(cp3, '--')
+    plot.plot(cp1, label='cp1')
+    plot.plot(cp2, label='cp2', linestyle='--')
+    plot.plot(cp3, label='cp3', linestyle='--')
+    plot.axhline(cp, label='cp', linestyle='--', color='m')
+    plot.legend()
+    plot.tight_layout()
+
     plot.show()
 
     assert res[-1] == cp
